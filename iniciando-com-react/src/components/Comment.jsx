b36af3bc5b97a822879e612d5d5148391a9281db
nandorsm <fernandocaetano933@gmail.com>
2022-12-29 08:01:43 -0300

    homeCommit01

---
+++ iniciando-com-react/src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.Comment}>
+            <img src="https://github.com/nandorsm.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Fernando Caetano</strong>
+                            <time title='26 de Dezembro às 22:24h' dateTime='2022-12-26 22:24:30'>Cerca de 1h atrás</time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+
+
+        </div>
+    );
+}
