b92d2c06b6c7177e03b6518037b63a6fdb53ff10
nandorsm <fernandocaetano933@gmail.com>
2023-01-04 01:32:08 -0300

    propriedades do Post

--- iniciando-com-react/src/App.jsx
+++ iniciando-com-react/src/App.jsx
@@ -5,6 +5,43 @@ import { Sidebar } from "./components/Sidebar";
 import "./global.css";
 import styles from "./App.module.css";
 
+//author: { avatar_url: "", nome: "", role: "" }
+//publishedAt: Date
+// content: String
+
+//iteração
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/nandorsm.png',
+      name: 'Fernando Caetano',
+      role: 'CTO @ Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare' },
+      { type:'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego3g',
+      role: 'Educator @ Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare' },
+      { type:'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-01-04 20:00:00'),
+  },
+];
+
 function App() {
   return (
     <div className="App">
@@ -13,11 +50,20 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post
+          {/* <Post
             author="Fernando Caetano"
             content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ullam quaerat commodi soluta dicta voluptatem"
           />
-          <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+          <Post author="Gabriel Buzzi" content="Um novo post muito legal" /> */}
+          {posts.map(post => {
+            return(
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
