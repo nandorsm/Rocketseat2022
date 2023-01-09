8d12ab202577303d2c9751e2f5d46f6d86080527
Fernando <fernandocaetano933@gmail.com>
2023-01-09 12:41:38 -0300

    Inserindo comentários

--- iniciando-com-react/src/components/Comment.jsx
+++ iniciando-com-react/src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment() {
+export function Comment( { content } ) {
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/nandorsm.png" alt="" />
@@ -18,7 +18,7 @@ export function Comment() {
                             <Trash size={24}/>
                         </button>
                     </header>
-
+                    <p>{content}</p>
                     <p>Muito bom Devon, parabéns!! 👏👏</p>
                 </div>
                 <footer>
--- iniciando-com-react/src/components/Post.jsx
+++ iniciando-com-react/src/components/Post.jsx
@@ -1,12 +1,22 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 
+
+
 export function Post( { author, publishedAt, content } ) {
+
+    const [comments, setComments] = useState([
+        'Post muito banaca, hein?'
+    ])
+
+    const [newCommentText, setNewCommentText] = useState('');
+
     const publishedDataFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
     })
@@ -15,6 +25,22 @@ export function Post( { author, publishedAt, content } ) {
         locale: ptBR,
         addSuffix: true
     })
+
+    function handleCreateNewComment() {
+        event.preventDefault();
+
+        const newCommentText = event.target.comment.value
+
+        setComments([...comments, newCommentText])
+        setNewCommentText(''); //declarativo
+
+        //event.target.comment.value = ''; //limpar comentário imperativamente
+    }
+
+    function handleNewCommentChange(){
+        setNewCommentText(event.target.value);
+
+    }
 
     return(
         <article className={styles.post}>
@@ -42,10 +68,13 @@ export function Post( { author, publishedAt, content } ) {
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
                 <textarea
+                    name='comment'
                     placeholder='Deixe um comentário'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
                 <footer>
                     <button type='submit'>Publicar</button>
@@ -53,9 +82,9 @@ export function Post( { author, publishedAt, content } ) {
             </form>
 
             <div className={styles.commentList}>
-                <Comment/>
-                <Comment/>
-                <Comment/>
+                {comments.map(comment =>{
+                    return <Comment content={comment}/>
+                })}
             </div>
 
 
