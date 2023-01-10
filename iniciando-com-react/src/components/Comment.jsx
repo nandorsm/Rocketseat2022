2fc1d27aa2f6ebb6de06c3e5b9caa3dc14502aa9
nandorsm <fernandocaetano933@gmail.com>
2023-01-10 00:51:35 -0300

    Removendo Comentários

--- iniciando-com-react/src/components/Comment.jsx
+++ iniciando-com-react/src/components/Comment.jsx
@@ -2,7 +2,13 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment( { content } ) {
+export function Comment( { content, onDeleteComment } ) {
+
+    function handleDeleteComment() {
+        console.log('deletar')
+        onDeleteComment(content);
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/nandorsm.png" alt="" />
@@ -14,7 +20,7 @@ export function Comment( { content } ) {
                             <strong>Fernando Caetano</strong>
                             <time title='26 de Dezembro às 22:24h' dateTime='2022-12-26 22:24:30'>Cerca de 1h atrás</time>
                         </div>
-                        <button title='Deletar comentário'>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size={24}/>
                         </button>
                     </header>
--- iniciando-com-react/src/components/Post.jsx
+++ iniciando-com-react/src/components/Post.jsx
@@ -39,7 +39,14 @@ export function Post( { author, publishedAt, content } ) {
 
     function handleNewCommentChange(){
         setNewCommentText(event.target.value);
+    }
 
+    function DeleteComment(commentToDelete){
+        //imutabilidade → as variáveis não sofrem mutação, criamos um novo valor(um novo espaço na memória)
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        })
+        setComments(commentsWithoutDeletedOne);
     }
 
     return(
@@ -61,9 +68,9 @@ export function Post( { author, publishedAt, content } ) {
             <div className={styles.content}>
                 {content.map(line => {
                     if (line.type == 'paragraph'){
-                        return <p>{line.content}</p>
+                        return <p key={line.content}>{line.content}</p>
                     }else if (line.type == 'link'){
-                        return <p><a href="#">{line.content}</a></p>
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
                     }
                 })}
             </div>
@@ -83,7 +90,13 @@ export function Post( { author, publishedAt, content } ) {
 
             <div className={styles.commentList}>
                 {comments.map(comment =>{
-                    return <Comment content={comment}/>
+                    return (
+                        <Comment 
+                            key={comment}
+                            content={comment}
+                            onDeleteComment={DeleteComment}
+                        />
+                    )
                 })}
             </div>
 
