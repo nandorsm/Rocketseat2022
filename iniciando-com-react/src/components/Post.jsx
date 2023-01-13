428eeae8d302cbc278685dbbfc6aa3a5e3986175
nandorsm <fernandocaetano933@gmail.com>
2023-01-13 01:14:10 -0300

    Validando Formulário

--- iniciando-com-react/src/components/Post.jsx
+++ iniciando-com-react/src/components/Post.jsx
@@ -38,7 +38,12 @@ export function Post( { author, publishedAt, content } ) {
     }
 
     function handleNewCommentChange(){
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório!')
     }
 
     function DeleteComment(commentToDelete){
@@ -48,6 +53,8 @@ export function Post( { author, publishedAt, content } ) {
         })
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length == 0;
 
     return(
         <article className={styles.post}>
@@ -82,9 +89,13 @@ export function Post( { author, publishedAt, content } ) {
                     placeholder='Deixe um comentário'
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button type='submit' disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
